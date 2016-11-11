6c00eba6d8f346e1b42a1abbf1d249789a2a9c00
hebowei <iorange@126.com>
2016-11-11 14:30:22 +0800

    try some tf-idf similarity

--- TF-IDF_Similarity.py
+++ TF-IDF_Similarity.py
@@ -9,7 +9,50 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 from ExcelFileFeed import ExcelFileIn
 
-def pre_processing(sentence):
+
+class TfIdfHelper:
+    """
+    Take a list of documents as the knowledge. When input some sentence, return the TfIdf value of each word.
+    """
+    def __init__(self, tokenizer, document_lines=None, document_file_path=None):
+        if document_file_path is not None:
+            xlsFile = ExcelFileIn(document_file_path)
+            self.document_lines = xlsFile.get_lines()
+        elif document_lines is not None:
+            self.document_lines = document_lines
+        else:
+            raise Exception('Error: Should define either document path or document string list, provided none.')
+        self.tokenizer = tokenizer
+        self.tfidf = TfidfVectorizer(tokenizer=self.tokenizer, stop_words='english')
+        self.tfidf.fit_transform(self.document_lines)
+
+
+    def get_non_zero_tfidf(self, sentence):
+        '''
+        Input a sentence and return non-zero tf-idf values
+        :param sentence:
+        :return: dictionary, {term: val}
+        '''
+        _, stem_dict = stem_with_translation_dict(pre_processing(sentence, stem=False))
+        response = self.tfidf.transform([sentence])
+        feature_names = self.tfidf.get_feature_names()
+        tfidf_dict = {}
+        for col in response.nonzero()[1]:
+            tfidf_dict[stem_dict[feature_names[col]]] = response[0, col]
+        return tfidf_dict
+
+
+def stem_with_translation_dict(tokens):
+    stemmed = []
+    stem_dict = {}
+    stemmer = PorterStemmer()
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+        stem_dict[stemmed[-1]] = item
+    return stemmed, stem_dict
+
+
+def pre_processing(sentence, stem=True):
     pattern = r'<a.*>.*</a>'
     sentence = re.sub(pattern, "", sentence)
     pattern = r'((http|ftp|https):\/\/)?[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?'
@@ -21,6 +64,8 @@ def pre_processing(sentence):
     # Stop Word Removal
     tokens = [word for word in tokens if not word in stopwords.words('english')]
     #tokens = Counter(tokens)
+    if not stem:
+        return tokens
 
     # Stem Tokens
     stemmed = []
